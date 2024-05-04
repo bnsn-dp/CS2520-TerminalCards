845f7984fb03d32a27d6532ca8f8d82ca3365a0f
Benson Diep <bdiep45@gmail.com>
2024-05-03 19:19:43 -0700

    Expanded "help" command

--- main.py
+++ main.py
@@ -1,34 +1,45 @@
-# How to implement terminal-like commands in python
-# 1. Take the command as string input
-# 2. Split the string into a tuple
-# 3. Read the first item as the core command
-# 4. Feed the remaining elements of the tuple into an appropriate parameter
 import random
+
+COMMANDS = {
+  "help"    : "Prints list of all commands AND Prints usage for given command",
+  "decks"   : "Prints list of all decks",
+  "current" : "Prints name of selected deck",
+  "cards"   : "Prints list of all cards in selected deck",
+  "select"  : "Selects a deck",
+  "shuffle" : "Randomizes order of cards in selected deck",
+  "draw"    : "Prints the question face of the card at the top of the deck",
+  "flip"    : "Prints the answer face of the most recently drawn card",
+  "add"     : "Adds a card to a deck",
+  "edit"    : "Changes either the question or answer of the most recently drawn card",
+  "remove"  : "Deletes a card or deck",
+  "quit"    : "Exits the program"
+}
 
 master_pile = {} #dictionary of all the cards in the deck. Unordered by default
 queue = [] # an empty list that will hold the key-value pairs in a given order. Can be emptied, so we don't want to use the master_pile here
-master_file = None #file to keep track of current deck
+master_file = "" #file to keep track of current deck
+
 def process(raw_command: str):
   segments = raw_command.split(" ", 1)
   command = segments[0]
-  flags = segments[1] if len(segments )> 1 else ""
+  flags = segments[1] if len(segments) > 1 else ""
   match command:
     case "help":
-      help()
+      help(flags)
     case "decks":
       decks(flags)
     case "current":
-      current(flags)
+      current()
     case "cards":
       cards(flags)
     case "select":
       select(flags)
     case "shuffle":
-      shuffle(flags)
+      shuffle()
     case "draw":
-      draw(flags)
+      draw()
     case "flip":
-      flip(flags)
+      flip()
     case "add":
       add(flags)
     case "quit":
@@ -38,18 +49,52 @@ def process(raw_command: str):
       print("unrecognized command")
       print()
 
-def help():
+def help(flags):
   print()
-  print("\t{:<12} {}".format("help", "Prints list of all commands"))
-  print("\t{:<12} {}".format("decks", "Prints list of all decks"))
-  print("\t{:<12} {}".format("current", "Prints current working deck"))
-  print("\t{:<12} {}".format("cards[!]", "Prints list of all cards in current working deck"))
-  print("\t{:<12} {}".format("select", "Changes current working deck"))
-  print("\t{:<12} {}".format("shuffle[!]", "Randomizes order of cards in current working deck"))
-  print("\t{:<12} {}".format("draw[!]", "Prints the question of the card at the top of the current working deck"))
-  print("\t{:<12} {}".format("flip[!]", "Prints the answer of the most recently drawn card"))
-  print("\t{:<12} {}".format("add[!]", "Adds a card to a deck"))
-  print("\t{:<12} {}".format("quit", "Exits the program"))
+  if len(flags.split(" ")) > 1:
+    print("\n\tToo many flags. \"help\" command takes only one flag [command]")
+    return
+  match flags:
+    case "decks":
+      print("\tUSAGE: \"decks\"")
+    case "current":
+      print("\tUSAGE: \"current\"")
+    case "cards":
+      print("\tUSAGE: \"cards\" [--sort | -s <ascend> | <descend]")
+      print("\tFLAG: [--sort | -s] Prints cards sorted in ascending or descending order")
+      print("\tNOTE: Needs a selected deck")
+    case "select":
+      print("\tUSAGE: \"select\" [deck]")
+      print("\tFLAG: [deck] specifies which deck to choose")
+    case "shuffle":
+      print("\tUSAGE: \"shuffle\"")
+      print("\tNOTE: Needs a selected deck")
+    case "draw":
+      print("\tUSAGE: \"draw\"")
+      print("\tNOTE: Needs a selected deck")
+    case "flip":
+      print("\tUSAGE: \"flip\"")
+      print("\tNOTE: Needs a recently drawn card")
+    case "add":
+      print("\tUSAGE: \"add\" [question]")
+      print("\tNOTE: Needs a selected deck")
+    case "edit":
+      print("\tUSAGE: \"edit\" [<question> | <answer>]")
+      print("\tNOTE: Needs a recently drawn card")
+    case "remove":
+      print("\tUSAGE: \"remove\" [<deck> | <card>]")
+      print("\tNOTE: Needs a recently drawn card")
+      print("\tNOTE: Needs a selected deck")
+    case "quit":
+      print("\tUSAGE: \"quit\"")
+    case "":
+      print("\tUSAGE: \"help\" [command]")
+      print("\tFLAGS: [command] Prints usage and flags of [command]\n")
+      for command, definition in COMMANDS.items():
+        print("\t{:<12} {}".format(command, definition))
+    case _:
+      print("Unrecognized Command")
+  
   print()
 
 def decks(flags):
@@ -64,13 +109,13 @@ def decks(flags):
   except FileNotFoundError:
     print(f"No decks found. Create a deck with the 'select' command")
 
-def current(flags):
+def current():
   print(master_file)
 
 def cards(flags):
   # Prints out the contents of the master_file
   # Flags:
-  #   [--sort | -s <name | size> <ascend | descend>]: Sorts the cards with the specified rules
+  #   [--sort | -s <name> <ascend | descend>]: Sorts the cards with the specified rules
   #     For example: "cards --sort name ascend" would return the decks sorted by name in alphabetical order
   split = flags.split()
   if len(split) == 0:
@@ -106,10 +151,10 @@ def select(flags):
     master_file = flags #changing the current working deck
     file.close()
     update_master_pile() # This will populate master_pile with the cards so the other functions can use it
-    print(f"Deck {flags} selected")
+    print(f"\tDeck {flags} selected")
   except FileNotFoundError: #no file so we ask to create it
     print()
-    answer = input("Deck not found. Do you want to create it? (yes/no): ")
+    answer = input("\tDeck not found. Do you want to create it? (yes/no): ")
     if answer == "yes":
       master_file = flags #changing the current working deck
       file = open(flags, "a") #creating file
@@ -120,36 +165,36 @@ def select(flags):
       file.write(master_file) #adding to list of decks
       file.close()
 
-def shuffle(flags):
+def shuffle():
   # Randomizes the order of the current master_pile
   keys = list(master_pile.keys())
   random.shuffle(keys)
   for key in keys:
-    print(f"{key}: {master_pile[key]}")
+    print(f"\t{key}: {master_pile[key]}")
 
-def draw(flags):
+def draw():
   # Pops and prints a key off the top of the current master_pile
   if master_pile:
-    card_key, card_value = master_pile.popitem(last=False)
-    print("Question:", card_key)
+    card_key, card_value = master_pile.popitem() # removed parameter: last=False
+    print("\tQuestion:", card_key)
   else:
-    print("No cards left in the deck.")
+    print("\tNo cards left in the deck.")
 
-def flip(flags):
+def flip():
   # Prints the value of the current key
   global master_pile # Accessing Global variable master_pile
   if master_pile:
     current_card_key = list(master_pile.keys())[-1] # Accessing the current card in the master_pile.
-    print("Answer:", master_pile[current_card_key]) # Prints the answer to the current card
+    print("\tAnswer:", master_pile[current_card_key]) # Prints the answer to the current card
   else:
-    print("No cards have been drawn yet.")
+    print("\tNo cards have been drawn yet.")
 
 def add(flags):
   if master_file is None: #making sure we are using a deck
-    print(" No deck selected")
-    print(" Use command 'select' to select/add a deck. Use command 'help' for list of commands")
+    print("\tNo deck selected")
+    print("\tUse command 'select' to select/add a deck. Use command 'help' for list of commands")
   else:
-    answer = input("Enter answer for %s: " % flags) #getting answer for the key/question
+    answer = input("\tEnter answer for %s: " % flags) #getting answer for the key/question
     #writing to file
     file = open(master_file, "a")
     file.write("%s : %s\n" % (flags, answer))
@@ -167,7 +212,7 @@ def update_master_pile():
       master_pile[key] = value
     file.close()
   else:
-    print(f"No deck selected. Use command 'select' to select/add a deck. Use command 'help' for list of commands")
+    print(f"\tNo deck selected. Use command 'select' to select/add a deck. Use command 'help' for list of commands")
 
 def print_banner():
   print("TerminalCards (TC) by B.Diep, M. Nasla, J. Rodas, M. Yaskowitz")
@@ -176,11 +221,10 @@ def print_banner():
 def main():
   print_banner()
   print("Welcome to TerminalCards! Type \"help\" to get started\n")
-  root = "User@CS2520"
-  working_deck = ""
+  root = "User@CS2520\\"
   prompt_marker = ":~$ "
   while True:
-    command = input(root + working_deck + prompt_marker)
+    command = input(root + master_file + prompt_marker)
     process(command)
 
 main()
