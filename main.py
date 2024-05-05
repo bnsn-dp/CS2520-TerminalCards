cf16fbac2b17a55692e802c3f930ccd629730957
Benson Diep <bdiep45@gmail.com>
2024-05-05 10:06:53 -0700

    Added ascii banner

--- main.py
+++ main.py
@@ -205,7 +205,13 @@ def update_master_pile():
     print(f"\tNo deck selected. Use command 'select' to select/add a deck. Use command 'help' for list of commands")
 
 def print_banner():
-  print("TerminalCards (TC) by B.Diep, M. Nasla, J. Rodas, M. Yaskowitz")
+  print("""  _______                  _             _  _____              _     
+ |__   __|                (_)           | |/ ____|            | |    
+    | | ___ _ __ _ __ ___  _ _ __   __ _| | |     __ _ _ __ __| |___ 
+    | |/ _ \\ '__| '_ ` _ \\| | '_ \\ / _` | | |    / _` | '__/ _` / __|
+    | |  __/ |  | | | | | | | | | | (_| | | |___| (_| | | | (_| \\__ \\
+    |_|\\___|_|  |_| |_| |_|_|_| |_|\\__,_|_|\\_____\\__,_|_|  \\__,_|___/""")
+  print("\n\t(TC) by B.Diep, M. Nasla, J. Rodas, M. Yaskowitz")
   print()
 
 def main():
